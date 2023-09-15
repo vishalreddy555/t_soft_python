72ec65717e1086a50572ce98ef63380bfef8d5cd
vishalreddy555 <vishal.vp0622@gmail.com>
2023-09-15 14:57:43 +0530

    t_soft_python

---
+++ Basics/datatypes/data_types.py
@@ -0,0 +1,5 @@
+var_1=5
+var_2=15.5
+print(var_1+var_2)
+print(type(var_1))
+print(type(var_2))
---
+++ Basics/datatypes/type_casting.py
@@ -0,0 +1,6 @@
+first_number=input("enter a value:")
+second_number=input("enter a value:")
+
+sum=int(first_number) + int(second_number)
+print(sum)
+
